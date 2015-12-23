2f8fad020f12ed77fb63b3319926f7b491174ca8
Mojca Žilavec <mojca.zilavec@gmail.com>
2015-12-23 10:56:22 +0100

    Novo

--- lib/libraries.r
+++ lib/libraries.r
@@ -1,6 +1,7 @@
 require(dplyr)
 require(rvest)
 require(gsubfn)
+library(knitr)
 library(dplyr)
 library(reshape2)
 library (ggplot2)
@@ -10,3 +11,4 @@ source("lib/xml.r", encoding = "UTF-8")
 
 # Uvozimo funkcije za pobiranje in uvoz zemljevida.
 source("lib/uvozi.zemljevid.r", encoding = "UTF-8")
+
