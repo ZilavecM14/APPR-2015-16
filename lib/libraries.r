038838f82744b3392081d185dd1c6db4ce90f506
Mojca Žilavec <mojca.zilavec@gmail.com>
2015-12-23 10:43:07 +0100

    Novo

--- lib/libraries.r
+++ lib/libraries.r
@@ -1,6 +1,9 @@
-library(knitr)
-library(ggplot2)
+require(dplyr)
+require(rvest)
+require(gsubfn)
 library(dplyr)
+library(reshape2)
+library (ggplot2)
 
 # Uvozimo funkcije za delo z datotekami XML.
 source("lib/xml.r", encoding = "UTF-8")
